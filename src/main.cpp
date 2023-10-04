b725f807142a640a87d400ef98841d85025a4967
nth@terminal <nth@terminal.hell>
2023-10-04 16:38:34 +0800

    triangle :3

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,21 @@
 #include <cmath>
 #include <format>
 
-int width = 600; 
-int height = 600;
-char name[] = "meow!";
+int width = 800; 
+int height = 800;
+char name[] = "teapot";
 
 void RenderModel(SDL_Renderer* renderer, const modelr& model, float modelShrink, float xOff, float yOff) {
     const std::vector<std::vector<int>> faces = model.GetFaces();
     const std::vector<modelr::Vertex>& vertices = model.GetVertices();
 
     for (const std::vector<int>& faceIndices : faces) {
-        for (size_t i = 0; i < faceIndices.size(); ++i) {
+        const modelr::Vertex& v0 = vertices[faceIndices[0] - 1];
+        for (size_t i = 0; i < faceIndices.size() - 1; ++i) {
                 
-            const modelr::Vertex& v0 = vertices[faceIndices[i] - 1];
-            const modelr::Vertex& v1 = vertices[faceIndices[(i + 1) % faceIndices.size()] - 1 ]; // thank you unnick <3
-            const modelr::Vertex& v2 = vertices[faceIndices[(i + 2) % faceIndices.size()] - 1 ];
+            const modelr::Vertex& v1 = vertices[faceIndices[i] - 1 ]; // thank you unnick + dunkyl <3
+            const modelr::Vertex& v2 = vertices[faceIndices[i+1] - 1 ];
                 
-            // const Vertex& v3 = vertices[faceIndices[(i + 3) % faceIndices.size()] -1 ];
-            // printlog(0, "obj", std::format("Processing {} / {}, Vertex; {} {} / {} {} / {} {}", i, faceIndices.size(), v0.x, v0.y, v1.x, v1.y, v2.x,v2.y).data());
-
             float x0 = (((v0.x + xOff) * width / modelShrink) + width / 2);
             float y0 = ((-(v0.y + yOff) * height / modelShrink) + height / 2);
                 
@@ -37,33 +34,23 @@ void RenderModel(SDL_Renderer* renderer, const modelr& model, float modelShrink,
             float x2 = (((v2.x + xOff) * width / modelShrink) + width / 2);
             float y2 = ((-(v2.y + yOff) * height / modelShrink) + height / 2);
 
-            // float x3 = ((v3.x * width / 6.0) + width / 2);
-            // float y3 = (((v3.y - 1.5) * height / 6.0) + height / 2);
-
-            SDL_Vertex sv0 = {{x0,y0},{255,255,255},{1,1}};
-            SDL_Vertex sv1 = {{x1,y1},{255,255,255},{1,1}};
-            SDL_Vertex sv2 = {{x2,y2},{255,255,255},{1,1}};
-            // SDL_Vertex sv3 = {{x3,y3},{rand()%255,rand()%255,rand()%255},{1,1}};
+            SDL_Vertex sv0 = {{x0,y0},{255,0,0},{1,1}};
+            SDL_Vertex sv1 = {{x1,y1},{0,255,0},{1,1}};
+            SDL_Vertex sv2 = {{x2,y2},{0,0,255},{1,1}};
 
             SDL_Vertex sv_vertices[] = {
                 sv0,
                 sv1,
                 sv2,
-                // sv3
             };
-
-            // Render the line segment between the transformed vertices
-            // SDL_RenderDrawLine(renderer, x0,y0,x1,y1);
-            // nvm fuck dat shit we making geometry
                 
-            SDL_RenderGeometry(renderer,nullptr,sv_vertices,3   ,NULL,0);  
+            SDL_RenderGeometry(renderer,nullptr,sv_vertices,3,NULL,0);
         }
     }
 }
 
 int main(int argc, char ** argv)
 {   
-
     printlog(0, "init", "initializing SDL!");
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
@@ -85,7 +72,7 @@ int main(int argc, char ** argv)
     SDL_Window *window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                           width,
                                           height,
-                                          SDL_WINDOW_OPENGL);
+                                          SDL_WINDOW_RESIZABLE);
     if (window == nullptr)
     {
         printlog(3, "init", "could not create window!");
